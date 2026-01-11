e334232eb8c793390a9b7969c707bc8537bc4acf
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-11 13:11:47 +0530

    Port bind to 0.0.0.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 
 const PORT = process.env.PORT || "5000";
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
